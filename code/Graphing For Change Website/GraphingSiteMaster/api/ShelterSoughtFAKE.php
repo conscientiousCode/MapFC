7698f1f99c82a6832d6576220b0d1a2939ad0b9f
Daniel Herman <conscientiouscode@outlook.com>
2018-03-27 10:54:00 -0700

    Distribution tested and configured to generate fake data for Shelter Sought

--- code/Graphing For Change Website/GraphingSiteMaster/api/ShelterSoughtFAKE.php
+++ code/Graphing For Change Website/GraphingSiteMaster/api/ShelterSoughtFAKE.php
@@ -12,19 +12,7 @@ include_once $_SERVER["DOCUMENT_ROOT"].'/data_gen/Random.php';
 include_once $_SERVER["DOCUMENT_ROOT"].'/data_gen/json/GoogleJsonFormatter.php';
 include_once $_SERVER["DOCUMENT_ROOT"].'/data_gen/json/JsonValidator.php';
 
-function waitListDistribution($day,$spread){
-    $centre = 365;
-    $centreNext = (-1)*(365-$centre);
-    return exp(-$spread*pow($day -$centre,2)) + exp(-$spread*pow($day-$centreNext,2));
-}
 
-function random(){
-    return mt_rand()/mt_getrandmax();
-}
-
-function waitList($day,$perterbation,$population, $spread){
-    return floor(waitListDistribution($day,$spread)*(0.5+ $perterbation*random()*0.5)*$population);
-}
 
 $shelterDesireRate = 1/10;
 $fluctuationPerDay = 1/40;
@@ -32,7 +20,7 @@ $population = 1500;
 
 $winterSol = new WinterDistribution($shelterDesireRate, $fluctuationPerDay, $population);
 
-$waitlistProb = 1/30;
+$waitlistProb = 1/8;
 $waitlistPerterbation = 1/50;
 $shelterWait = new ShelterWaitlistDistribution($waitlistProb, $waitlistPerterbation, $population);
 
@@ -46,8 +34,7 @@ for($i = 1; $i <= 365; $i++){
     $row = array(
         0=> $i,
         1=> $winterSol->valueForDay($i),
-        2=> waitList($i, 1/3, $population/5, 1/500) + 20+10*random()
-        //2=>$selterWait->valueForDay($i) +20+10*random();
+        2=>$shelterWait->valueForDay($i) +20+10*random()
     );
     //echo $row[0]."\n".$row[1]."\n";
     $jsonFormatter->addRow($row);
